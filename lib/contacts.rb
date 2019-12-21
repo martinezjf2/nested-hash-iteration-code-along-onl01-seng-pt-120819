778a1b8dd3e2a1afdf6d12ee4e0b46b39998d7c1
Jeffrey Martinez <martinezjf2@gmail.com>
2019-12-21 18:14:40 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -23,6 +23,7 @@ def remove_strawberry(contacts)
   if attribute == :favorite_ice_cream_flavors
   # binding.pry
       data.delete_if {|ice_cream| ice_cream == "strawberry"}
+    end
 end
     end
 end
